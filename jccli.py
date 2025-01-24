1e476101e6ff6c3ee3e7db3cef3e7a0d481fca5a
sganyunchi@163.com <78394473+gi-b716@users.noreply.github.com>
2025-01-24 22:05:59 +0800

    Using "logging"

--- jccli.py
+++ jccli.py
@@ -1,7 +1,15 @@
-import socket,datetime,time,os
+import socket,datetime,time,os,logging
 
 print("CWD:"+os.getcwd())
-LogFile=f"KeyLog{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.log"
+logger = logging.getLogger('logger')
+logger.setLevel(logging.DEBUG)
+# logFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logFormatter = logging.Formatter('')
+logFile = logging.FileHandler(f"KeyLog{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.log")
+logFile.setLevel(logging.DEBUG)
+logFile.setFormatter(logFormatter)
+logger.addHandler(logFile)
+logger.info("Init logger")
 
 # print(socket.gethostbyname(""))
 
@@ -20,7 +28,7 @@ while True:
         nowtime=datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S:%f")
         msg=f"[{nowtime}:FROM{str(addr)} ] == {data.decode("utf-8")}"
         print(msg)
-        open(LogFile,'a').write(msg+"\n")
+        logger.info(msg)
     except Exception as e:
         msg=str(e)
         print(e)
